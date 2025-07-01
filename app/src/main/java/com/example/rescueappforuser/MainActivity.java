d9d0233add8cc9296c8f93be4f82b7db849ba73f
CodeExorxiS <kgabrez8@gmail.com>
2025-07-02 03:08:35 +0800

    update strings

--- app/src/main/java/com/example/rescueappforuser/MainActivity.java
+++ app/src/main/java/com/example/rescueappforuser/MainActivity.java
@@ -15,51 +15,15 @@ import com.example.rescueappforuser.databinding.OnetimepasswordactivityBinding;
 import com.example.rescueappforuser.databinding.SignUpPageBinding;
 import com.example.rescueappforuser.databinding.SplashScreenActivityBinding;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private static final int Splash_delay = 2000;
-    private ActivityMainBinding root;
-    private OnetimepasswordactivityBinding binding;
-    private SplashScreenActivityBinding splashBinding;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        splashBinding = SplashScreenActivityBinding.inflate(getLayoutInflater());
-        setContentView(splashBinding.getRoot());
-
-
-        new Handler().postDelayed(() -> {
-            root = ActivityMainBinding.inflate(getLayoutInflater());
-            setContentView(root.getRoot());
-
-            root.btnLogin.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    String userText = root.edtUsername.getText().toString().trim();
-                    String passText = root.edtPassword.getText().toString().trim();
-
-                    if (userText.isEmpty()) {
-                        root.edtUsername.setError("Input Username");
-                        root.edtUsername.requestFocus();
-                    } else if (passText.isEmpty()) {
-                        root.edtPassword.setError("Input Password");
-                        root.edtPassword.requestFocus();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-
-            root.txtSignUp.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
-                    startActivity(intent);
-                }
-            });
-        }, Splash_delay);
+        setContentView(R.layout.activity_main);
     }
 }
